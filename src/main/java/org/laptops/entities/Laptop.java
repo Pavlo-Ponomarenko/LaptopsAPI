ad3cb24c51dfe94b7f5f1d235cc2e118453d7ad7
Pavlo Ponomarenko <reactor2345@gmail.com>
2024-04-29 22:08:34 +0300

    add liquibase scheme

--- src/main/java/org/laptops/entities/Laptop.java
+++ src/main/java/org/laptops/entities/Laptop.java
@@ -1,22 +1,22 @@
 package org.laptops.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "laptops")
 @NoArgsConstructor
 @Setter
 @Getter
 public class Laptop {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.REMOVE)
     private Producer producer;
     private String processor;
     private String memory;
--- src/main/java/org/laptops/entities/Producer.java
+++ src/main/java/org/laptops/entities/Producer.java
@@ -2,11 +2,13 @@ package org.laptops.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "producers")
 @NoArgsConstructor
 @Setter
 @Getter
